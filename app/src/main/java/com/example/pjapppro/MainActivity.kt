6d40e07e9827b2033777999b7a8d801ba5f77b5d
Ukiyyy <uros.mravljak@student.um.si>
2023-05-17 13:07:09 +0200

    added QR code scanner

--- app/src/main/java/com/example/pjapppro/MainActivity.kt
+++ app/src/main/java/com/example/pjapppro/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.pjapppro
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnQRscanner(view: View) {
+        onScanQRcode(view)
+    }
+    var getData = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            println("rezultati")
+        }
+    }
+    fun onScanQRcode(view: View) {
+        try {
+            val intent = Intent("com.google.zxing.client.android.SCAN")
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE")
+            getData.launch(intent)
+        } catch (e: Exception) {
+            println("neuspesno")
+            var marketUri = Uri.parse("market://details?id=com.google.zxing.client.android")
+            val marketIntent = Intent(Intent.ACTION_VIEW, marketUri)
+            startActivity(marketIntent)
+        }
     }
 }
