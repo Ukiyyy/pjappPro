9cce0afe5a68b0ad9f8fc8d1d1a12f773ea8ad0a
TinKlancnik <tin.klancnik@student.um.si>
2023-05-26 08:01:48 +0200

    login done

--- app/src/main/java/com/example/pjapppro/LoginActivity.kt
+++ app/src/main/java/com/example/pjapppro/LoginActivity.kt
@@ -1,12 +1,60 @@
 package com.example.pjapppro
-
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pjapppro.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var buttonLogin: Button
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        // Add your login activity implementation here
+
+        editTextUsername = findViewById(R.id.editTextUsername)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        buttonLogin = findViewById(R.id.buttonLogin)
+
+        firestore = FirebaseFirestore.getInstance()
+
+        buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString().trim()
+            val password = editTextPassword.text.toString().trim()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                loginUser(username, password)
+            }
+        }
+    }
+
+    private fun loginUser(username: String, password: String) {
+        firestore.collection("users")
+            .whereEqualTo("username", username)
+            .whereEqualTo("password", password)
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val documents = task.result?.documents
+                    if (documents != null && documents.isNotEmpty()) {
+                        // Login successful
+                        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                        // You can navigate to the desired activity here
+                    } else {
+                        // Login failed
+                        Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // Login failed
+                    Toast.makeText(this, "Login failed. Please try again.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
